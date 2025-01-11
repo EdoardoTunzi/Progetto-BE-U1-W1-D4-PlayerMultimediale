39b6c9709e6dbf4e869b37973efba5332e8eb06e
Edoardo <edfl.tunzi@gmail.com>
2025-01-11 17:01:26 +0100

    metodo per gestire volume implementato, bug fixes. PLAYER COMPLETO

--- src/MediaPlayer.java
+++ src/MediaPlayer.java
@@ -2,6 +2,8 @@ import be.progetto.classi.ElementoMultimediale;
 import be.progetto.classi.Immagine;
 import be.progetto.classi.RegistrazioneAudio;
 import be.progetto.classi.Video;
+import be.progetto.interfacce.Luminosità;
+import be.progetto.interfacce.Riproducibile;
 
 import java.util.Scanner;
 
@@ -32,7 +34,7 @@ public class MediaPlayer {
 
         Scanner scanner = new Scanner(System.in);
         ElementoMultimediale[] elementi = new ElementoMultimediale[5];
-
+        //----------------------------------sezione creazione elementi multimediali-----------------------------------
         System.out.println("Accensione Player");
         for (int i = 0; i < elementi.length ; i++) {
             System.out.println("Crea il " + (i+1) +"° elemento multimediale:");
@@ -82,7 +84,7 @@ public class MediaPlayer {
         if(elementi.length == 5) {
             System.out.println(" 5 elementi creati con successo!");
         }
-
+        //-------------------------------------sezione scelta riproduzione-------------------------------------------
         int sceltaElemento;
 
         do {
@@ -95,17 +97,29 @@ public class MediaPlayer {
             System.out.println("Inserisci 0 per uscire dal player");
 
             sceltaElemento = scanner.nextInt();
+
            if (sceltaElemento >=1 && sceltaElemento <= 5) {
                ElementoMultimediale elementoSelezionato = elementi[sceltaElemento - 1];
-
+                //downcasting necessario per accedere ai metodi delle istanze delle sottoclassi
                if ( elementoSelezionato instanceof Immagine) {
+                   //richiesta di modifica valori prima di esecuzione con metodo( a fine codice) per maggiore leggibilità, visto che era troppo lungo
+                  gestioneLuminosità((Luminosità) elementoSelezionato, scanner);
+                   System.out.println("Eseguo " + elementoSelezionato.titolo);
                    ((Immagine) elementoSelezionato).show();
+
                } else if (elementoSelezionato instanceof Video) {
+                   gestioneVolume((Riproducibile) elementoSelezionato, scanner);
+                   gestioneLuminosità((Luminosità) elementoSelezionato, scanner);
+                   System.out.println("Eseguo " + elementoSelezionato.titolo);
                    ((Video) elementoSelezionato).play();
-               } else {
+
+               } else if (elementoSelezionato instanceof RegistrazioneAudio) {
+                   gestioneVolume((Riproducibile) elementoSelezionato, scanner);
+                   System.out.println("Eseguo " + elementoSelezionato.titolo);
                    ((RegistrazioneAudio) elementoSelezionato).play();
                }
-           } else if(sceltaElemento !=0) {
+
+           } else if(sceltaElemento !=0) { //controllo necessario perchè senza mi stampava questo messaggio anche se terminavo il player con 0
                System.out.println("Scelta non valida, Riprova inserendo un numero da 1 a 5 per selezionare un elemento.");
            }
         } while (sceltaElemento !=0);
@@ -113,4 +127,72 @@ public class MediaPlayer {
         System.out.println("Player spento. Programma terminato.");
 
     }
-}
+    //--------------------------------metodi per gestione luminosità e durata parte utente------------------------
+
+
+    //utilizzo l'interfaccia come type per l'elemento selezionato così il metodo può essere utilizzato sia da Immagine che Video
+
+    public static void gestioneLuminosità(Luminosità elementoSelezionato, Scanner scanner) {
+        System.out.println("Vuoi modificare la luminosità prima di eseguirlo? ");
+        System.out.println("1 = Si , 2 = No");
+        int selezione = scanner.nextInt();
+
+
+        if (selezione == 1) {
+            System.out.println("Inserisci: 1 - per aumentare la luminosità, 2 - per ridurre la luminosità");
+            int selezLum = scanner.nextInt();
+            if (selezLum == 1) {
+                System.out.println("Inserisci valore numerico per aumentare la luminosità");
+                int newLum1 = scanner.nextInt();
+                elementoSelezionato.aumentaLuminosita(newLum1);
+
+            } else if (selezLum == 2) {
+                System.out.println("Inserisci valore numerico per ridurre la luminosità");
+                int newLum2 = scanner.nextInt();
+                elementoSelezionato.diminuisciLuminosita(newLum2);
+
+            }
+
+
+        } else if (selezione == 2) {
+            System.out.println("Passo a Riproduzione...");
+            //Tentativo di riproduzione inutile, perchè è meglio gestire la riproduzione dopo l'invocazione di questo metodo, che deve gestire solo la luminosità.
+            /*if (elementoSelezionato instanceof Immagine) {
+                ((Immagine) elementoSelezionato).show();
+            } else if (elementoSelezionato instanceof Video) {
+                ((Video) elementoSelezionato).play();
+            }*/
+
+        } else {
+            System.out.println("Selezione non valida");
+        }
+    }
+
+    //metodo simile ma con Riproducibile come parametro, per gestione volume.
+    public static void gestioneVolume(Riproducibile elementoSelezionato, Scanner scanner) {
+        System.out.println("Vuoi modificare il volume prima di eseguirlo? ");
+        System.out.println("1 = Si , 2 = No");
+        int selezione = scanner.nextInt();
+
+
+        if (selezione == 1) {
+            System.out.println("Inserisci: 1 - per aumentare volume, 2 - per ridurre volume");
+            int selezVol = scanner.nextInt();
+
+            if (selezVol == 1) {
+                System.out.println("Inserisci valore numerico per aumentare volume");
+                int newVol1 = scanner.nextInt();
+                elementoSelezionato.alzaVolume(newVol1);
+            } else if (selezVol == 2) {
+                System.out.println("Inserisci valore numerico per ridurre volume");
+                int newVol2 = scanner.nextInt();
+                elementoSelezionato.abbassaVolume(newVol2);
+            }
+
+        } else if (selezione == 2) {
+            System.out.println("Ok continuo");
+        } else {
+            System.out.println("Selezione non valida");
+        }
+    }
+ }
